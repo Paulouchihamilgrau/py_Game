ca079d0ad87b95fc0d4b4ed51b826904239ff7bc
Paulo Eduardo <83376346+Paulouchihamilgrau@users.noreply.github.com>
2023-07-08 16:18:16 -0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 import pygame
 pygame.init()
 
+x = 400
+y = 300
+velocidade = 5
+
 largura = 800
 altura = 600
 tela = pygame.display.set_mode((largura, altura))
@@ -9,16 +13,25 @@ pygame.display.set_caption("Meu Jogo")
 
 executando = True
 while executando:
-    
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             executando = False
 
-    
+        comandos = pygame.key.get_pressed()
+    if comandos[pygame.K_UP]:
+            y-= velocidade
+    if comandos[pygame.K_DOWN]:
+            y+= velocidade
+    if comandos[pygame.K_RIGHT]:
+            x+= velocidade
+    if comandos[pygame.K_LEFT]:
+            x-= velocidade
+
     tela.fill((0, 0, 0))
 
     
-    pygame.display.flip()
-
+    pygame.draw.circle(tela,(0,255,0),(x,y),50)
+    pygame.display.update()
 
 pygame.quit()
