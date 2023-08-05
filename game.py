42840760164fa4f3da029fe5d1a3953193548abb
Paulo Eduardo <83376346+Paulouchihamilgrau@users.noreply.github.com>
2023-08-05 13:30:28 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,24 +1,25 @@
 import pygame
-from pygame.locals import *
 from sys import exit
 from random import randint
 
+
 pygame.init()
 
-musica_de_fundo= pygame.mixer.music.load('musica_legal_mp3.wav')
+
+pygame.mixer.music.load('musica_legal_mp3.wav')
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 
-x = 400
-y = 300
-velocidade = 20
-
 largura = 800
 altura = 600
+x = int(largura/2)
+y = int(altura/2)
+
+x_azul = randint(40,600)
+y_azul = randint(50, 430)
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Meu Jogo")
-
 
 executando = True
 while executando:
@@ -27,23 +28,27 @@ while executando:
         if event.type == pygame.QUIT:
             executando = False
 
-        comandos = pygame.key.get_pressed()
+    comandos = pygame.key.get_pressed()
     if comandos[pygame.K_UP]:
-            y-= velocidade
+        y -= 20
     if comandos[pygame.K_DOWN]:
-            y+= velocidade
+        y += 20
     if comandos[pygame.K_RIGHT]:
-            x+= velocidade
+        x += 20
     if comandos[pygame.K_LEFT]:
-            x-= velocidade
+        x -= 20
 
     tela.fill((255, 255, 255))
 
-    
-    ret_verde = pygame.draw.rect(tela,(0,255,0),(x,y,40,50))
-    ret_vermelho =  pygame.draw.rect(tela,(255,0,0),(200,300,40,50) )
+    ret_verde = pygame.draw.rect(tela, (0, 255, 0), (x, y, 40, 50))
+    ret_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x_azul, y_azul, 40, 50))
+
+    if ret_verde.colliderect(ret_vermelho):
+        x_azul = randint(40, 600)
+        y_azul = randint(50, 430)
 
     pygame.display.flip()
 
 
 pygame.quit()
+
